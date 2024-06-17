945f7581d103c20eff8fa8364151f2b671322dc7
BITAN CHOWDHURY <121960216+Bitan1803@users.noreply.github.com>
2024-06-17 22:20:48 +0000

    variables declare

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 144552
+let accountEmail = "bitan@google.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword,accountCity]);
