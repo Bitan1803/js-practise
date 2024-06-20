2bedbcdee921679c3d381b65ec6f920b32a4a8f6
BITAN CHOWDHURY <121960216+Bitan1803@users.noreply.github.com>
2024-06-20 10:31:26 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,21 @@
+const myArr = [0,4,5,2,4,7,]
+console.log(myArr[4]);
+
+//myArr.push(6)
+//myArr.push(7)
+//myArr.pop()
+
+//myArr.unshift(45)
+//myArr.shift(5)
+//console.log(myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("A", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+console.log("B", myArr);
+
+
+
